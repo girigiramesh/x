fcddd28b86dee2402d371a15db7f7abc8a3ff9c7
Ramesh Girigi <ramesh@kvana.com>
2020-01-18 16:49:04 +0530

    test y

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,11 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Test purpose
+    // Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // test
 
     }
 }
