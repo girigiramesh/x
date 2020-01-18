5f62093acc0e5c2fe8759902d75f601b67b92648
Ramesh Girigi <ramesh@kvana.com>
2020-01-18 16:46:36 +0530

    test y

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Test
+    // Test purpose
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
